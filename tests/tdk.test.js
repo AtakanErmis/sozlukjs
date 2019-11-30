a17c5f08ab7263b4573d92295dccb40d6d6157ab
Atakan Ermiş <ataerms@gmail.com>
2019-11-30 23:55:33 +0300

    tussling with test stuff

--- tests/tdk.test.js
+++ tests/tdk.test.js
@@ -1,19 +1,22 @@
 const TDKDictionary = require("../src/modules/tdk");
 
-test("gets meaning JSON data successfully", () => {
+test("gets meaning JSON data successfully", (done) => {
 	TDKDictionary.getMeaningData("sözlük").then(data => {
 		expect(data).toBeInstanceOf(Object);
+		done();
 	});
 });
 
-test("gets spelling JSON data successfully", () => {
+test("gets spelling JSON data successfully", (done) => {
 	TDKDictionary.getSpellingData("sözlük").then(data => {
 		expect(data).toBeInstanceOf(Object);
+		done();
 	});
 });
 
-test("gets audio JSON data successfully", () => {
+test("gets audio JSON data successfully", (done) => {
 	TDKDictionary.getAudioUrl("sözlük").then(data => {
 		expect(data).toBeInstanceOf(Object);
+		done();
 	});
 });
--- tests/tureng.test.js
+++ tests/tureng.test.js
@@ -1,13 +1,15 @@
 const TurengDictionary = require("../src/modules/tureng");
 
-test("gets term JSON data successfully", () => {
+test("gets term JSON data successfully", (done) => {
     TurengDictionary.getTermData("dictionary").then(data => {
 		expect(data).toBeInstanceOf(Object);
+		done();
 	});
 })
 
-test("gets suggestion JSON data successfully", async () => {
+test("gets suggestion JSON data successfully", (done) => {
     TurengDictionary.getTermSuggestions("dictionary").then(data => {
 		expect(data).toBeInstanceOf(Object);
+		done();
 	});
 })
