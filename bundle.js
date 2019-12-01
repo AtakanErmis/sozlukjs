ba460b5d858ba2787d20a59b44b48a1004eb1e21
Atakan Ermiş <ataerms@gmail.com>
2019-12-01 12:54:10 +0300

    Release 1.0.11

--- bundle.js
+++ bundle.js
@@ -86,7 +86,7 @@ class TurengDictionary {
 				method: "POST",
 				headers: {
 					"Content-Type": "application/json",
-					Origin: "tureng.com"
+					"Origin": "tureng.com"
 				},
 				body: JSON.stringify(requestData)
 			}
