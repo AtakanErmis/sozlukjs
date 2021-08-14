3f12db0c0bfefb88f43a373b0743c366e66ee41f
Atakan Ermiş <ataerms@gmail.com>
2021-08-14 16:20:56 +0300

    2.0 initial

--- bundle.js
+++ bundle.js
@@ -1,5 +1,5 @@
 require=(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
-(function (global){
+(function (global){(function (){
 "use strict";
 
 // ref: https://github.com/tc39/proposal-global
@@ -18,12 +18,14 @@ var global = getGlobal();
 module.exports = exports = global.fetch;
 
 // Needed for TypeScript and Webpack.
-exports.default = global.fetch.bind(global);
+if (global.fetch) {
+	exports.default = global.fetch.bind(global);
+}
 
 exports.Headers = global.Headers;
 exports.Request = global.Request;
 exports.Response = global.Response;
-}).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
+}).call(this)}).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
 },{}],2:[function(require,module,exports){
 const fetch = require("node-fetch");
 
@@ -81,7 +83,7 @@ class TurengDictionary {
 			Term: term
 		};
 		const request = await fetch(
-			"http://cors-passer.herokuapp.com/http://ws.tureng.com/TurengSearchServiceV4.svc/Search",
+			"http://ws.tureng.com/TurengSearchServiceV4.svc/Search",
 			{
 				method: "POST",
 				headers: {
