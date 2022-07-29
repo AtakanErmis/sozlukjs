cf36282fe0adb4cc9c54e9205f0c92d89ea3b43c
amar-jay <abdelmanan.abdelrahman03@gmail.com>
2022-07-30 01:34:02 +0300

    Testing and fixing bugs

---
+++ dist/tryout.js
@@ -0,0 +1,8 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const index_1 = __importDefault(require("./index"));
+index_1.default.TurengDictionary.getTermData('hello');
+//# sourceMappingURL=tryout.js.map
--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,3 @@
-export {};
+import TDKDictionary from "src/modules/tdk.d";
+import TurengDictionary from "src/modules/tureng.d";
+export { TDKDictionary, TurengDictionary  };
